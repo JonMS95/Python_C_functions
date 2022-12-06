b36527dce463e6571bc6bce9b796afa64b93c3c2
JonMS95 <jon_portugalete@hotmail.com>
2022-12-06 17:50:33 +0100

    Added some lines where the C code is called. Added also some code where the input types are specifed.

--- Python_Files/usingCFunctions.py
+++ Python_Files/usingCFunctions.py
@@ -7,3 +7,12 @@ from usingCFunctions_constants import PATH_CHECK_SIGN, PATH_SQUARE
 
 C_checkIntSign = ctypes.CDLL(PATH_CHECK_SIGN)
 C_squareNumber = ctypes.CDLL(PATH_SQUARE)
+
+# The type of the arguments that are going to be passed to the functions within the .so files should be previously
+# specified. Therefore, the Python code itself will cast it to the specified C data type.
+C_checkIntSign.function.argtypes = [ctypes.c_int]
+C_squareNumber.function.argtypes = [ctypes.c_int]
+
+print(C_checkIntSign.function(-4))
+print(C_checkIntSign.function(5))
+print(C_checkIntSign.function(0))
