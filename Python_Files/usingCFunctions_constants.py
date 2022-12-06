9a201b5e257aebc9d1136aa3a6db9aefca26d1b6
JonMS95 <jon_portugalete@hotmail.com>
2022-12-06 17:49:59 +0100

    Modified PATH_SO_FILES constants. It seems that Python doesn't like absolute paths with "~", but with the whole user home directory name.

--- Python_Files/usingCFunctions_constants.py
+++ Python_Files/usingCFunctions_constants.py
@@ -1,3 +1,3 @@
-PATH_SO_FILES   = "~/Desktop/scripts/Python/Python_C_functions/C_Files/C_Dynamic_libraries/" 
+PATH_SO_FILES   = "/home/jon/Desktop/scripts/Python/Python_C_functions/C_Files/C_Dynamic_libraries/" 
 PATH_CHECK_SIGN = PATH_SO_FILES + "libcheckIntSign.so"
 PATH_SQUARE     = PATH_SO_FILES + "libsquareNumber.so"
