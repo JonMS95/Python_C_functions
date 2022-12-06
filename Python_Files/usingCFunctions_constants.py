3bcb375f066dee5562d65cd6b69575ec1cb9a7ff
JonMS95 <jon_portugalete@hotmail.com>
2022-12-06 19:19:54 +0100

    Added TEST_INTEGER_AMOUNT constant, which tells how many integers have to be added in the "square" function for both languages.

--- Python_Files/usingCFunctions_constants.py
+++ Python_Files/usingCFunctions_constants.py
@@ -1,3 +1,5 @@
 PATH_SO_FILES   = "/home/jon/Desktop/scripts/Python/Python_C_functions/C_Files/C_Dynamic_libraries/" 
 PATH_CHECK_SIGN = PATH_SO_FILES + "libcheckIntSign.so"
 PATH_SQUARE     = PATH_SO_FILES + "libsquareNumber.so"
+
+TEST_INTEGER_AMOUNT = 100
