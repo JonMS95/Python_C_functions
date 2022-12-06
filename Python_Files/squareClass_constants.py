d54bd040e2b8de461d5f2d96abc1feabe7b20dbd
JonMS95 <jon_portugalete@hotmail.com>
2022-12-06 18:50:26 +0100

    Added square class, which contains the methods that are meant to be tested against the ones that have been previously written in C.

---
+++ Python_Files/squareClass_constants.py
@@ -0,0 +1,2 @@
+STR_SQUARE_INIT = "This class contains two methods, that are going to be tested against equivalent C functions."
+STR_SQUARE_STR  = "Let's check this class functions against C code ones!"
